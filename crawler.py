42e621218f58f5c3a9bc458b9fa263a6deca4013
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2018-05-29 20:25:57 +0200

    jupyter notebook, encoding fixed, some sentiment tools tried

--- crawler.py
+++ crawler.py
@@ -11,6 +11,11 @@ consumer_secret1 = 'tHAdc255ukdTiQlgu2ScDsNcMdN19hwxMH38AZCgRlJABs54Jj'
 access_token1 = '1000699980087267330-gLDPsw7NuYl2zA31DNwZhLXpWe8tvi'
 access_token_secret1 = 'MT3TVDHiFoVOf0fI4oggLLgXpad1cBIDaD3Znv0H0UQqN'
 
+consumer_key1 = '2txDFLO3JRcivufznuRJZWZKO'
+consumer_secret1 = 'PBHCAuuWafmzz05hyj8zjIEVQUsgzQozWXo6V3AZcwLcr3pArj'
+access_token1 = '1000730493434855424-lZU6guxqzbNFJ2LIwjnPCe9Dgny1dz'
+access_token_secret1 = 'F6LxC7Bj4tAvcTwhEjzPVJbpQmMxk1QkUCnUQVE64c8s3'
+
 auth = tweepy.OAuthHandler(consumer_key1, consumer_secret1)
 auth.set_access_token(access_token1, access_token_secret1)
 api = tweepy.API(auth)
